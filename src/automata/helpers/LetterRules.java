8f6deaf6153dd6cc9d2ec5d7d3203cc57b01dbef
Matheus Rogge Cocia de Oliveira <matheus-rogge@hotmail.com>
2019-04-07 00:25:15 -0300

    Remove redudant loop

--- src/automata/helpers/LetterRules.java
+++ src/automata/helpers/LetterRules.java
@@ -9,31 +9,13 @@ public class LetterRules {
 
     private static final int LOWER_CASE_CHARACTERS_ASCII_TABLE_OFFSET = 0x61;
 
-    private static List<Character> getUpperCaseCharacterRules() {
+    public static List<Character> getRules() {
         ArrayList<Character> transitions = new ArrayList<>();
 
         for (int i = 0; i < 26; i++) {
             transitions.add((char) (UPPER_CASE_CHARACTERS_ASCII_TABLE_OFFSET + i));
-        }
-
-        return transitions;
-    }
-
-    private static List<Character> getLowerCaseCharacterRules() {
-        ArrayList<Character> transitions = new ArrayList<>();
-
-        for (int i = 0; i < 26; i++) {
             transitions.add((char) (LOWER_CASE_CHARACTERS_ASCII_TABLE_OFFSET + i));
         }
-
-        return transitions;
-    }
-
-    public static List<Character> getRules() {
-        ArrayList<Character> transitions = new ArrayList<>();
-
-        transitions.addAll(getUpperCaseCharacterRules());
-        transitions.addAll(getLowerCaseCharacterRules());
 
         return transitions;
     }
