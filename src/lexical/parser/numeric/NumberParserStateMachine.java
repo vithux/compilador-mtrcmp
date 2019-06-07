27035cac0dd0555ccb6a46d7198964b0a80daaa9
Matheus Rogge Cocia de Oliveira <matheus-rogge@hotmail.com>
2019-06-06 22:28:42 -0300

    Fix number parser

--- src/lexical/parser/numeric/NumberParserStateMachine.java
+++ src/lexical/parser/numeric/NumberParserStateMachine.java
@@ -14,8 +14,10 @@ import automata.helpers.AcceptNumbersState;
 class NumberParserStateMachine {
 
     private static final char FLOAT_SEPARATOR = '.';
+    private static final char EXPONENTIATION_MINUS_SIGN = '-';
     private static final char EXPONENTIATION_PLUS_SIGN = '+';
     private static final char BASE_TEN_EXPONENTIATION_TOKEN = 'E';
+    private static final char BASE_TEN_EXPONENTIATION_TOKEN_ALT = 'e';
 
     private static final State INITIAL_STATE;
 
@@ -54,20 +56,25 @@ class NumberParserStateMachine {
 
         // Transitions
 
-        FLOAT_FINAL_STATE.addTransition(baseTenExponentiationTransition(FLOAT_BASE_TEN_EXPONENTIATION_STATE));
+        FLOAT_FINAL_STATE.addTransition(new Transition(BASE_TEN_EXPONENTIATION_TOKEN, FLOAT_BASE_TEN_EXPONENTIATION_STATE));
+        FLOAT_FINAL_STATE.addTransition(new Transition(BASE_TEN_EXPONENTIATION_TOKEN_ALT, FLOAT_BASE_TEN_EXPONENTIATION_STATE));
 
         FLOAT_BASE_TEN_EXPONENTIATION_STATE.addTransition(new Transition(EXPONENTIATION_PLUS_SIGN, FLOAT_EXPONENTIATION_SIGN_STATE));
         FLOAT_BASE_TEN_EXPONENTIATION_STATE.addTransition(new Transition(EXPONENTIATION_PLUS_SIGN, FLOAT_EXPONENTIATION_SIGN_STATE));
+
+        FLOAT_BASE_TEN_EXPONENTIATION_STATE.addTransition(new Transition(EXPONENTIATION_MINUS_SIGN, FLOAT_EXPONENTIATION_SIGN_STATE));
+        FLOAT_BASE_TEN_EXPONENTIATION_STATE.addTransition(new Transition(EXPONENTIATION_MINUS_SIGN, FLOAT_EXPONENTIATION_SIGN_STATE));
 
         INTEGER_FINAL_STATE.addTransition(new Transition(FLOAT_SEPARATOR, FLOAT_START_STATE));
-        INTEGER_FINAL_STATE.addTransition(baseTenExponentiationTransition(INTEGER_BASE_TEN_EXPONENTIATION_STATE));
+
+        INTEGER_FINAL_STATE.addTransition(new Transition(BASE_TEN_EXPONENTIATION_TOKEN, INTEGER_BASE_TEN_EXPONENTIATION_STATE));
+        INTEGER_FINAL_STATE.addTransition(new Transition(BASE_TEN_EXPONENTIATION_TOKEN_ALT, INTEGER_BASE_TEN_EXPONENTIATION_STATE));
 
         INTEGER_BASE_TEN_EXPONENTIATION_STATE.addTransition(new Transition(EXPONENTIATION_PLUS_SIGN, INTEGER_EXPONENTIATION_SIGN_STATE));
         INTEGER_BASE_TEN_EXPONENTIATION_STATE.addTransition(new Transition(EXPONENTIATION_PLUS_SIGN, INTEGER_EXPONENTIATION_SIGN_STATE));
-    }
 
-    private static Transition baseTenExponentiationTransition(State nextState) {
-        return new Transition(BASE_TEN_EXPONENTIATION_TOKEN, nextState);
+        INTEGER_BASE_TEN_EXPONENTIATION_STATE.addTransition(new Transition(EXPONENTIATION_MINUS_SIGN, INTEGER_EXPONENTIATION_SIGN_STATE));
+        INTEGER_BASE_TEN_EXPONENTIATION_STATE.addTransition(new Transition(EXPONENTIATION_MINUS_SIGN, INTEGER_EXPONENTIATION_SIGN_STATE));
     }
 
     public static NumericType getNumericTypeFromFinalState(FiniteStateMachine stateMachine) {
