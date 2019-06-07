b3cbd166c2049cbec3dcc648a32c806f39ede4c6
Vitor Rodrigues de marques <vitor@MacBook-Pro-de-Vitor.local>
2019-06-07 14:35:19 -0300

    add header

--- src/error/DuplicatedIdentifierError.java
+++ src/error/DuplicatedIdentifierError.java
@@ -1,3 +1,10 @@
+/**
+ * @author Matheus Rogge Cocia de Oliveira
+ * @author Rafael Bená Cineis
+ * @author Vitor Rodrigues de Marques
+ * @author Vitor Augusto da Silva Brandão
+ */
+
 package error;
 
 public class DuplicatedIdentifierError extends Error {
